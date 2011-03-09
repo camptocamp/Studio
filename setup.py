ffa62d9b774271fd9b6cd58401e855f21307f3ca
Eric Lemoine <eric.lemoine@camptocamp.com>
2011-03-09 10:55:53 +0100

    get prepared for the 0.5 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = open('README.rst').read()
 
 setup(
     name                    = 'Studio',
-    version                 = '0.5dev',
+    version                 = '0.5',
     license                 = 'GPLv3',
     description             = 'Web-based administration interface for MapServer',
     long_description        = long_description,
